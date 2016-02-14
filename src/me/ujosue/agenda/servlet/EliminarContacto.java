7966695d9631595cebaebcd5d0e373dcf38c46c6
UJosue <estuardo1915@hotmail.com>
2016-02-14 11:27:58 -0600

    1.214.4b
    Función agregada para eliminar contactos

---
+++ src/me/ujosue/agenda/servlet/EliminarContacto.java
@@ -0,0 +1,32 @@
+package me.ujosue.agenda.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import me.ujosue.agenda.bean.Contacto;
+import me.ujosue.agenda.bean.Usuario;
+import me.ujosue.agenda.db.Conexion;
+
+public class EliminarContacto extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res)throws IOException,ServletException{
+		HttpSession sesion= (HttpSession) req.getSession();
+		Usuario user = (Usuario) sesion.getAttribute("usuario");
+		Integer idContacto =  new Integer(req.getParameter("idContacto"));
+		Contacto contacto = (Contacto) Conexion.getInstancia().buscar(Contacto.class, idContacto);
+		Conexion.getInstancia().eliminar(contacto);
+		req.setAttribute("listaContacto", Conexion.getInstancia().listar("FROM Contacto c where c.idUsuario='"+user.getIdUsuario()+"'"));
+		RequestDispatcher despachador=null;
+		despachador=req.getRequestDispatcher("agenda/inicio.jsp");
+		despachador.forward(req, res);
+}
+public void doGet(HttpServletRequest req, 
+		HttpServletResponse res)throws IOException,ServletException{
+	doPost(req,res);
+}
+}
